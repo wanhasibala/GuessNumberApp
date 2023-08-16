d8a93800a8d0933033f01fc23507f75ba24d62cf
wanhasibala <wanhasibal1122@gmail.com>
2023-08-16 10:44:49 +0700

    INITIAL COMMIT

---
+++ components/PrimaryButton.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+
+function PrimaryButton({ children }) {
+    return (<View>
+        <Text>{children}</Text>
+    </View>
+    );
+}
+
+export default PrimaryButton;
