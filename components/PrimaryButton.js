39cebc2b07885ad6fc6558c5ebe1c31a110bb164
wanhasibala <wanhasibal1122@gmail.com>
2023-08-16 16:12:59 +0700

    adding style for the startgamescreen

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 function PrimaryButton({ children }) {
     return (<View>
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,37 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton>Reset</PrimaryButton>
+                <PrimaryButton>Confirm</PrimaryButton>
+            </View>
         </View>
     )
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginHorizontal:24, 
+        marginTop: 100,
+        backgroundColor:'#72063c',
+        borderRadius: 10,
+        elevation: 4,
+        shadowColor:'black',
+        shadowOffset: { width: 0,height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    buttonContainer: {
+        gap: 10,
+        flexDirection: "row",
+        justifyContent: "center"
+    }
+});
