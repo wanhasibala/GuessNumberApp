725f480b7a130b768717178ceea0e455b20f2137
wanhasibala <wanhasibal1122@gmail.com>
2023-08-16 18:05:51 +0700

    adding style for the startgamescreen

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
-    return (<View>
-        <Text>{children}</Text>
-    </View>
+    function pressHandler(){
+        console.log('Pressed');
+    }
+
+    return (
+        <Pressable onPress={pressHandler}>
+            <View style={styles.container} >
+                <Text style={styles.text} >{children}</Text>
+            </View>
+        </Pressable>
     );
 }
 
 export default PrimaryButton;
+
+const styles=StyleSheet.create({
+    container:{
+        backgroundColor: "#72063c",
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        borderRadius: 8, 
+        elevation: 2,
+    },
+    text:{
+        color:"white"
+    }
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput style={styles.textInput} maxLength={2} keyboardType="number-pad" autoCorrect={false} />
             <View style={styles.buttonContainer}>
                 <PrimaryButton>Reset</PrimaryButton>
                 <PrimaryButton>Confirm</PrimaryButton>
@@ -21,13 +21,24 @@ const styles = StyleSheet.create({
         padding: 16,
         marginHorizontal:24, 
         marginTop: 100,
-        backgroundColor:'#72063c',
+        backgroundColor:'#ffffff',
         borderRadius: 10,
         elevation: 4,
         shadowColor:'black',
         shadowOffset: { width: 0,height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.25
+    },
+    textInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        color: '#ddb52f',
+        borderBottomWidth: 2,
+        borderBottomColor: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
     },
     buttonContainer: {
         gap: 10,
